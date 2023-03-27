9b4f1de8c76e86578bb878974dc7763b4b07053f
qasemB <mahdicmptr@gmail.com>
2023-03-28 02:00:17 +0330

    part #12

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,15 @@
+if ("serviceWorker" in navigator) {
+    navigator.serviceWorker.register("/sw.js")
+        .then(function(res){
+            console.log("SW registered...");
+            console.log(res);
+        })
+        .catch(function(e){
+            console.log(e.message);
+        })
+}
+
+
 document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.sidenav');
     var instances = M.Sidenav.init(elems, {

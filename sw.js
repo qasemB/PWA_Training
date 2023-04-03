712c86ed4051167830fa550743e6cf96ef39ecab
qasemB <mahdicmptr@gmail.com>
2023-04-04 01:28:48 +0330

    part #14

--- sw.js
+++ sw.js
@@ -1,3 +1,15 @@
-self.addEventListener("install", function(){
+self.addEventListener("install", function(e){
     console.log("SW installed...");
+    console.log(e);
 })  
+
+self.addEventListener("activate", function(e){
+    console.log("SW Activated...");
+    console.log(e);
+    return self.clients.claim()
+})
+
+self.addEventListener("fetch", function(e){
+    console.log("SW Fetching something...");
+    console.log(e);
+})

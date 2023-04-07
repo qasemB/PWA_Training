350068aaee827a8a84fbfd2a2ad9291ba79ae67c
qasemB <mahdicmptr@gmail.com>
2023-04-07 19:58:32 +0330

    part #15

--- assets/js/app.js
+++ assets/js/app.js
@@ -9,6 +9,35 @@ if ("serviceWorker" in navigator) {
         })
 }
 
+var deferredPrompt;
+window.addEventListener("beforeinstallprompt", (e) => {
+  console.log("beforeinstallprompt...");
+  e.preventDefault();
+  deferredPrompt = e;
+  console.log(e);
+  return false;
+});
+
+setTimeout(()=>{
+    document.getElementById('install-prompt').classList.add('show')
+},5000)
+
+document.getElementById("install-prompt").addEventListener("click", (e) => {
+  if (deferredPrompt) {
+    deferredPrompt.prompt();
+    e.target.classList.remove('show');
+    deferredPrompt.userChoice.then((choiceRes) => {
+      console.log(choiceRes.outcome);
+      if (choiceRes.outcome === "accepted") {
+        console.log("User accepted the install prompt.");
+      } else if (choiceRes.outcome === "dismissed") {
+        console.log("User dismissed the install prompt");
+      }
+    });
+    deferredPrompt = null;
+  }
+});
+
 
 document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.sidenav');

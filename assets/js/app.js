e70f027c02bbc1a1095cb07201d14eef0b1fc405
qasemB <mahdicmptr@gmail.com>
2023-04-13 10:55:20 +0330

    part #17

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,12 +1,13 @@
 if ("serviceWorker" in navigator) {
-    navigator.serviceWorker.register("/sw.js")
-        .then(function(res){
-            console.log("SW registered...");
-            console.log(res);
-        })
-        .catch(function(e){
-            console.log(e.message);
-        })
+  navigator.serviceWorker
+    .register("/sw.js")
+    .then(function (res) {
+      console.log("SW registered...");
+      console.log(res);
+    })
+    .catch(function (e) {
+      console.log(e.message);
+    });
 }
 
 var deferredPrompt;
@@ -18,14 +19,18 @@ window.addEventListener("beforeinstallprompt", (e) => {
   return false;
 });
 
-setTimeout(()=>{
-    document.getElementById('install-prompt').classList.add('show')
-},5000)
+window.addEventListener("DOMContentLoaded", () => {
+  if (!window.matchMedia("(display-mode: standalone)").matches) {
+    setTimeout(() => {
+      document.getElementById("install-prompt").classList.add("show");
+    }, 5000);
+  }
+});
 
 document.getElementById("install-prompt").addEventListener("click", (e) => {
   if (deferredPrompt) {
     deferredPrompt.prompt();
-    e.target.classList.remove('show');
+    e.target.classList.remove("show");
     deferredPrompt.userChoice.then((choiceRes) => {
       console.log(choiceRes.outcome);
       if (choiceRes.outcome === "accepted") {
@@ -38,10 +43,9 @@ document.getElementById("install-prompt").addEventListener("click", (e) => {
   }
 });
 
-
-document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.sidenav');
-    var instances = M.Sidenav.init(elems, {
-        edge: "right"
-    });
+document.addEventListener("DOMContentLoaded", function () {
+  var elems = document.querySelectorAll(".sidenav");
+  var instances = M.Sidenav.init(elems, {
+    edge: "right",
+  });
 });

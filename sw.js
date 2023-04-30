41c7055f5e0ab1813d3e9126927826927f03e90d
qasemB <mahdicmptr@gmail.com>
2023-04-30 21:10:14 +0330

    part #22

--- sw.js
+++ sw.js
@@ -10,23 +10,43 @@ let staticItems = [
     "https://fonts.gstatic.com/s/materialicons/v140/flUhRq6tzZclQEJ-Vdg-IuiaDsNcIhQ8tQ.woff2",
 ]
 
+let STATIC_CACHE = "static-v3"
+let DYNAMIC_CACHE = "dynamic"
+
+// install event -------------------------------------->>
 self.addEventListener("install", function(e){
     e.waitUntil(
-        caches.open("static").then(cache=>{
+        caches.open(STATIC_CACHE).then(cache=>{
             cache.addAll(staticItems)
         })
     )
 })  
 
+
+// activate event ------------------------------------>>
 self.addEventListener("activate", function(e){
+    e.waitUntil(
+        caches.keys().then(keys=>{
+            return Promise.all(
+                keys.map(key=>{
+                    if (key != STATIC_CACHE && key != DYNAMIC_CACHE) {
+                        console.log("Deleting Cache:", key);
+                        return caches.delete(key);
+                    }
+                })
+            )
+        })
+    )
     return self.clients.claim()
 })
 
+
+// fetch event -------------------------------------->>
 self.addEventListener("fetch", function(e){
     e.respondWith(
         caches.match(e.request).then(res=>{
             return res || fetch(e.request).then(fetchRes=>{
-                return caches.open("dynamic").then(cache=>{
+                return caches.open(DYNAMIC_CACHE).then(cache=>{
                     cache.put(e.request, fetchRes.clone())
                     return fetchRes
                 })

492e292960dbea5027ddae2bf99055fb8056e119
qasemB <mahdicmptr@gmail.com>
2023-07-08 22:47:15 +0330

    part #43

--- assets/js/media.js
+++ assets/js/media.js
@@ -1,4 +1,15 @@
+const videoElement = document.getElementById('media_player');
+const captureBtn = document.getElementById('media_capture_btn');
+const canvasElement = document.getElementById('media_canvas');
 
 navigator.mediaDevices.getUserMedia({video: true}).then(stream=>{
-    document.getElementById('media_player').srcObject = stream
+    videoElement.srcObject = stream
 })
+
+captureBtn.addEventListener('click', ()=>{
+    const context = canvasElement.getContext('2d');
+    canvasElement.classList.remove('dis-none')
+    videoElement.classList.add('dis-none')
+    context.drawImage(videoElement, 0 , 0, canvasElement.clientWidth, videoElement.videoHeight/(videoElement.videoWidth/canvasElement.width))
+})
+
